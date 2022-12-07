04ffad1f413358acbf1d010347b2c7fc05f0c2fc
4bysss <74374776+4bysss@users.noreply.github.com>
2022-12-07 12:59:25 +0100

    Add files via upload

--- Tema_8/usalectorEscritor.java
+++ Tema_8/usalectorEscritor.java
@@ -11,7 +11,7 @@ public class usalectorEscritor extends Thread {
     public void run(){
         switch(tipo){
             case 1:
-                for (long i = 0; i < 100000000; i++) {
+                for (long i = 0; i < 1000000; i++) {
                     le.iniciaLectura();
                     r.observer();
                     le.acabarLectura();
@@ -19,7 +19,7 @@ public class usalectorEscritor extends Thread {
                 break;
 
             case 2:
-                for (long i = 0; i < 100000000; i++) {
+                for (long i = 0; i < 1000000; i++) {
                     le.iniciarEscritura();
                     r.inc();
                     le.acabarEscritura();
@@ -27,7 +27,7 @@ public class usalectorEscritor extends Thread {
                 break;
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         recurso re = new recurso();
         lectorEscritor lec = new lectorEscritor();
         usalectorEscritor t1 = new usalectorEscritor(re, lec,1);
@@ -39,13 +39,21 @@ public class usalectorEscritor extends Thread {
         usalectorEscritor t7 = new usalectorEscritor(re, lec,2);
         usalectorEscritor t8 = new usalectorEscritor(re, lec,2);
         t1.start();
-        t5.start();
         t2.start();
-        t6.start();
         t3.start();
-        t7.start();
         t4.start();
+        t5.start();
+        t6.start();
+        t7.start();
         t8.start();
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+        t5.join();
+        t6.join();
+        t7.join();
+        t8.join();
         System.out.println(re.observer());
 
     }
